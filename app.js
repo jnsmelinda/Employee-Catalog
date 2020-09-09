57b28d4f23ce9e04252aaaa5275309335375f354
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-08 21:39:06 -0700

    moving db init to separate file

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-var mysql = require('mysql');
-const fs = require("fs");
+const mysql = require('mysql');
+const initdb = require('./db-init');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    multipleStatements: true
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    fs.readFile("seed.sql", "utf8", (err, data) => {
-        if (err) throw err;
-
-        connection.query(data, function (err, result) {
-            if (err) throw err;
-            console.log("Database initialized");
-            connection.destroy();
-        });
-    })
-});
+initdb();
---
+++ db-init.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+const fs = require("fs");
+
+async function initdb() {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        multipleStatements: true
+    });
+
+    connection.connect(function (err) {
+        if (err) throw err;
+        fs.readFile("seed.sql", "utf8", (err, data) => {
+            if (err) throw err;
+
+            connection.query(data, function (err, result) {
+                if (err) throw err;
+                console.log("Database initialized");
+                connection.destroy();
+            });
+        })
+    });
+}
+
+module.exports = initdb;
