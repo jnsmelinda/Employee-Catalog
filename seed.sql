db451e077ef4d801363b9ac560b2d0d147e17746
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-08 22:20:29 -0700

    create tables

--- seed.sql
+++ seed.sql
@@ -1,8 +1,31 @@
+-- DROP DATABASE employee_catalog;
+
 CREATE DATABASE IF NOT EXISTS employee_catalog;
 
 USE employee_catalog;
 
 CREATE TABLE IF NOT EXISTS department (
-    id INT,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
